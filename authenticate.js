395a41d61ea897dae4df224c18e05966f02de7ae
Amey <morje.amey@gmail.com>
2020-06-26 22:10:37 +0530

    Access Control for normal users and admin

--- authenticate.js
+++ authenticate.js
@@ -38,3 +38,16 @@ exports.jwtPassport = passport.use(new JwtStrategy(opts,
     }));
 
     exports.verifyUser = passport.authenticate('jwt', {session: false});
+
+    exports.verifyAdmin = (req, res , next)=>{
+        if(req.user.admin === true)  {
+            console.log("You are an admin!")
+            return next();
+        }
+        else {
+            var err = new Error('You are not authorized to perform this operation!');
+            err.status = 403;
+            return next(err);
+        }
+    };
+    
