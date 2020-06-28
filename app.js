b6028c3b3c64ac1718056854b66d9a84016d0635
Amey <morje.amey@gmail.com>
2020-06-29 01:07:25 +0530

    File Upload using Multer

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
+var uploadRouter = require('./routes/uploadRouter');
+
 
 const mongoose = require("mongoose");
 const Dishes = require('./models/dishes');
@@ -30,14 +32,14 @@ connect.then((db) =>{
 
 var app = express();
 
-app.all('*',(req,res,next)=>{
+/*app.all('*',(req,res,next)=>{
   if(req.secure){
     return next();
   }
   else {
     res.redirect(307, 'https://' + req.hostname + ':' + app.get('secPort') + req.url);
   }
-});
+});*/
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -61,6 +63,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
+app.use('/imageupload',uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
